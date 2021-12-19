1b4942bc1de25125333e390e916cb01a6102a165
YGunCoding <lkc637712@gmail.com>
2021-12-19 17:19:24 +0900

    AdminController.java 생성

--- src/main/java/com/reserve/restaruant/controller/AdminController.java
+++ src/main/java/com/reserve/restaruant/controller/AdminController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class AdminController {
 
-	
+	@GetMapping(value="/")
+	public String index() {
+		return "index";
+	}
 }
