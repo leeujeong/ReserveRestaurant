eaca1ad147e199fa00314a94e90164b9124254cc
YGunCoding <lkc637712@gmail.com>
2021-12-22 18:50:41 +0900

    커밋

--- src/main/java/com/reserve/restaruant/controller/AdminController.java
+++ src/main/java/com/reserve/restaruant/controller/AdminController.java
@@ -7,9 +7,7 @@ public class AdminController {
 
 public AdminController() {
 
-	System.out.println("test1");
-	System.out.println("test123 with suhwan");
-	System.out.println("test gun");
+
 	
 }
 	
